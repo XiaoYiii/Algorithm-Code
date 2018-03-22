07977957f8104054587671fbcee746c8fe029264
XiaoYiii <zhenhai.gl@gmail.com>
2018-03-23 00:51:50 +0800

    update
    update

--- LintCode/test.py
+++ LintCode/test.py
@@ -1,10 +1,29 @@
-import time
-starttime=time.time()
-#do something
-a=[1,2,3]
-a=a+[4]
-endtime=time.time()
-print(endtime-starttime)
 
 
-#2.1457672119140625e-06 1.9073486328125e-06
+
+# problem 1
+
+def cal(a,b):
+    alen=len(a)
+    blen=len(b)
+    ans=0
+    for i in range(0,alen-blen+1):
+        temp=a[i:i+blen]
+        for j in range(0,blen):
+            if temp[j]!=b[j]:
+                ans+=1
+    return ans
+
+if __name__=='__main__':
+    a=input()
+    b=input()
+    ans=cal(a,b)
+    print(ans)
+
+# dp=[0 for i in range(0,len(b))]
+# for p in range(len(temp)):
+#     for q in range(len(b)):
+#         if temp[p] == b[q]:
+#             dp[p + 1][q + 1] = dp[p][q] + 1
+#         else:
+#             dp[p + 1][q + 1] = max(dp[p + 1][q], dp[p][q + 1])
